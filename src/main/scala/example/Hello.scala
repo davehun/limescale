73b3e83713af12658b0a0fbc3891cc819c496718
David Hunnisett <david@inferiorplanet.co.uk>
2017-03-09 19:40:45 +0000

    pass url as arg through

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -9,17 +9,17 @@ import scala.concurrent.ExecutionContext.Implicits._
 object Hello {
 
   def main(args: Array[String]): Unit = {
-
+    val page: String = args(0)
     implicit val system = ActorSystem()
     implicit val materializer = ActorMaterializer()
     val wsClient = StandaloneAhcWSClient()
 
-    call(wsClient)
+    call(wsClient,page)
       .andThen { case _ => wsClient.close() }
       .andThen { case _ => system.terminate() }
   }
 
-  def call(wsClient: StandaloneAhcWSClient): Future[Unit] = {
+  def call(wsClient: StandaloneAhcWSClient, page: String): Future[Unit] = {
     wsClient.url("https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=jsonfm&titles=Java_virtual_machine").get().map { response =>
       val statusText: String = response.statusText
       val bodyText: String = response.body
